915207516a4942ba86e308026e0b9dad26a6a8ef
Sam Dunne <sam@sam-dunne.com>
2013-05-29 09:25:34 +0100

    Changed double-quotes to signle quotes were applicable

--- lib/omniauth-gplus.rb
+++ lib/omniauth-gplus.rb
@@ -1,5 +1,5 @@
-require "omniauth-oauth2"
-require_relative "omniauth/strategies/gplus"
-require_relative "omniauth/gplus/version"
+require 'omniauth-oauth2'
+require_relative 'omniauth/strategies/gplus'
+require_relative 'omniauth/gplus/version'
 
-OmniAuth.config.add_camelization("gplus", "GPlus")
+OmniAuth.config.add_camelization('gplus', 'GPlus')
